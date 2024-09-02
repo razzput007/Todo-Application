6bb4a74a2350e246e0e7861780e51acf26309ed4
razzput007 <dhoom.rk74@gmail.com>
2024-09-02 12:43:06 +0530

    Adding logic

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express=require('express');
 const { createTodo } = require('./type');
+const { todo } = require('./db');
 
 const app=express();
 
 app.use(express.json());
 const PORT=3000;
-app.post('/todo',(req,res)=>{
+app.post('/todo',async(req,res)=>{
     const user=req.body;
     const userParse=createTodo.safeParse(user);
     if(!userParse.success){
@@ -14,13 +15,26 @@ app.post('/todo',(req,res)=>{
         })
         return;
     }
+
+     await todo.create({
+        title:req.body.title,
+        description:req.body.description,
+        completed:false
+     })
+
+     res.json({
+        message:"Todo created successfully"
+     })
 })
 
-app.get('/todos',(req,res)=>{
-
+app.get('/todos',async(req,res)=>{
+    const todos=await todo.find({})
+    res.json({
+        todos
+    })
 })
 
-app.put('/completed',(req,res)=>{
+app.put('/completed',async(req,res)=>{
    const updateUser=req.body;
    const updateUserParse=createTodo.safeParse(updateUser);
    if(!updateUserParse.success){
@@ -29,6 +43,13 @@ app.put('/completed',(req,res)=>{
     })
     return;
    }
+   const _id=req.body.id;
+   const todo=await todo.findByIdAndUpdate(_id,{completed:true},{new:true})
+
+   res.json({
+    message:"Todo updated successfully"
+   })
+
 })
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
